662df0363efe751c3a122f6a02d127b545c4af5a
LouisCollinsJr <louiscollinsjr@marginal.space>
2016-11-30 09:31:22 -0600

    added todo ids

--- server.js
+++ server.js
@@ -2,11 +2,48 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function(req,res) {
+var todos = [{
+    id: 1,
+    description: 'Wash the car.',
+    completed: false
+}, {
+    id: 2,
+    description: 'Learn everything.',
+    completed: false
+}, {
+    id: 3,
+    description: 'Eat.',
+    completed: true,
+}];
+
+
+app.get('/', function (req, res) {
     res.send('TODO API Root.');
 });
 
+// GET /todos
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
 
-app.listen(PORT, function() {
-    console.log('Express listeing on port ' + PORT +'.');
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+    var todoID = parseInt(req.params.id, 10);
+    var matchedTodo;
+
+    todos.forEach(function (todo) {
+        if (todoID == todo.id) {
+            matchedTodo = todo;
+        }
+    });
+
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    } else {
+        res.status(400).send();
+    }
+});
+
+app.listen(PORT, function () {
+    console.log('Express listeing on port ' + PORT + '.');
 });
