6408812c9b446269dc5c6552ee74624015faac75
LouisCollinsJr <louiscollinsjr@marginal.space>
2016-11-30 16:04:38 -0600

    added delete

--- server.js
+++ server.js
@@ -38,11 +38,11 @@ app.get('/todos/:id', function (req, res) {
 
 app.post('/todos', function (req, res) {
     var body = _.pick(req.body, 'description', 'completed');
-        
-    if ( !_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
+
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
         return res.status(400).send();
     }
-   
+
     body.description = body.description.trim();
     console.log(body);
     body.id = todoNextId++;
@@ -51,7 +51,19 @@ app.post('/todos', function (req, res) {
     res.json(body);
 });
 
+// DELETE /todos/:id
 
+app.delete('/todos/:id', function (req, res) {
+    var todoID = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, { id: todoID });
+
+    if (!matchedTodo) {
+        res.status(404).json({ "error": "no todo found with that value" });
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    }
+});
 
 app.listen(PORT, function () {
     console.log('Express listeing on port ' + PORT + '.');
