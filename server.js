8d334afe1fbc6024e05283dc02f7710cd53c860b
LouisCollinsJr <louiscollinsjr@marginal.space>
2016-12-01 16:18:59 -0600

    DB post added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 var _ = require('underscore');
+var db = require('./db.js');
 
 app.use(bodyParser.json());
 var todos = [];
@@ -30,12 +31,12 @@ app.get('/todos', function (req, res) {
     } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
         filteredTodos = _.where(filteredTodos, { completed: false });
     }
-  // filteredTodos.indexOf(queryParams.q)
-  if ( queryParams.hasOwnProperty('q') && queryParams.q.length > 0 ) {
+    // filteredTodos.indexOf(queryParams.q)
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
         filteredTodos = _.filter(filteredTodos, function (todo) {
-             return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;  
-            });
-    } 
+            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+        });
+    }
 
     res.json(filteredTodos);
 });
@@ -58,17 +59,24 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {
     var body = _.pick(req.body, 'description', 'completed');
 
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
+   
+        db.todo.create(body).then(function (todo) {
+            res.json(todo.toJSON());
+        }).then(function (e) {
+            return res.status(400).json(e);
+        });
 
-    body.description = body.description.trim();
-    console.log(body);
-    body.id = todoNextId++;
-    todos.push(body);
-    //console.log('description:' + body.description);
-    res.json(body);
-});
+        // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        //     return res.status(400).send();
+        // }
+
+        // body.description = body.description.trim();
+        // console.log(body);
+        // body.id = todoNextId++;
+        // todos.push(body);
+        // //console.log('description:' + body.description);
+        // res.json(body);
+    });
 
 // DELETE /todos/:id
 
@@ -116,6 +124,8 @@ app.put('/todos/:id', function (req, res) {
     res.json(matchedTodo);
 });
 
-app.listen(PORT, function () {
-    console.log('Express listeing on port ' + PORT + '.');
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log('Express listeing on port ' + PORT + '.');
+    });
 });
