f83b64b67d68e4b094e43ff5cb80162a85485956
LouisCollinsJr <louiscollinsjr@marginal.space>
2016-11-30 21:52:35 -0600

    final update

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.get('/', function (req, res) {
 });
 
 // GET /todos
+// todos?completed=true&q=work
+
 app.get('/todos', function (req, res) {
     var queryParams = req.query;
     var filteredTodos = todos;
@@ -28,13 +30,14 @@ app.get('/todos', function (req, res) {
     } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
         filteredTodos = _.where(filteredTodos, { completed: false });
     }
+  // filteredTodos.indexOf(queryParams.q)
+  if ( queryParams.hasOwnProperty('q') && queryParams.q.length > 0 ) {
+        filteredTodos = _.filter(filteredTodos, function (todo) {
+             return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;  
+            });
+    } 
 
     res.json(filteredTodos);
-
-    // console.log(JSON.parse(queryParams));
-
-
-
 });
 
 // GET /todos/:id
