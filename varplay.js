577d376e390de0628458dff2132181d7aaacc633
LouisCollinsJr <louiscollinsjr@marginal.space>
2016-11-30 20:03:01 -0600

    add var playground

---
+++ varplay.js
@@ -0,0 +1,25 @@
+// var person = {
+//     name: 'Andrew',
+//     age: 21
+// };
+
+// function updatePerson(obj) {
+//     // obj = {
+//     //     name: 'Andrew',
+//     //     age: 24
+//     // };
+//     obj.age = 24;
+// }
+
+var grade = [12,13,14];
+
+function addGrade(array) {
+    array.push(12);
+    degugger;
+}
+//updateGrade(grade);
+//console.log(person);
+// console.log(grade);
+
+ addGrade(grade);
+  console.log(grade);
