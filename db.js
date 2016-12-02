cc42a6214b65639ba1d3476a7b9e44470c90d1e5
LouisCollinsJr <louiscollinsjr@marginal.space>
2016-12-02 08:18:29 -0600

    add psotgres

--- db.js
+++ db.js
@@ -1,12 +1,21 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-    'dialect': 'sqlite',
-    'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if (env === 'production') {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres'
+    });
+} else {
+    var sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect': 'sqlite',
+        'storage': __dirname + '/data/dev-todo-api.sqlite'
+    });
+}
 
 var db = {}; // object
 
-db.todo = sequelize.import(__dirname +'/models/todo.js');
+db.todo = sequelize.import(__dirname + '/models/todo.js');
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
