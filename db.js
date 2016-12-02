0189c996073f95a1985a4ff2c35543d8a7822263
LouisCollinsJr <louiscollinsjr@marginal.space>
2016-12-02 08:58:39 -0600

    add dialect ssl

--- db.js
+++ db.js
@@ -4,7 +4,10 @@ var sequelize;
 
 if (env === 'production') {
     sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres'
+        dialect: 'postgres',
+        dialectOptions: {
+    ssl: true
+  }
     });
 } else {
     sequelize = new Sequelize(undefined, undefined, undefined, {
