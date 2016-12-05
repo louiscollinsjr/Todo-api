afbd0696c76ad7280c1d9614adeea78c6659a94d
LouisCollinsJr <louiscollinsjr@marginal.space>
2016-12-05 16:51:19 -0600

    update complete

--- server.js
+++ server.js
@@ -95,33 +95,35 @@ app.delete('/todos/:id', function (req, res) {
 // PUT /todos/:id
 
 app.put('/todos/:id', function (req, res) {
-    var todoID = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, { id: todoID });
+    var todoId = parseInt(req.params.id, 10);
+    
     var body = _.pick(req.body, 'description', 'completed');
-    var validAttributes = {};
+    var attributes = {};
 
-
-    if (!matchedTodo) {
-        return res.status(404).send();
-    }
 
     //body.hasownproperty('completed');
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-        validAttributes.completed = body.completed;
-    } else if (body.hasOwnProperty('completed')) {
-        return res.status(400).send();
-    }
+    if (body.hasOwnProperty('completed') ) {
+        attributes.completed = body.completed;
+    } 
 
     //body.hasownproperty('description');
-    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-        validAttributes.description = body.description;
-    } else if (body.hasOwnProperty('description')) {
-        return res.status(400).send();
-    }
+    if (body.hasOwnProperty('description') ) {
+        attributes.description = body.description;
+    } 
 
-    //Ready to update
-    _.extend(matchedTodo, validAttributes);
-    res.json(matchedTodo);
+    db.todo.findById(todoId).then(function (todo) { 
+        if(todo) {
+            todo.update(attributes).then(function(todo) {
+        res.json(todo.toJSON());
+    }, function(e) {
+        res.status(400).json(e);
+    });
+        } else {
+            res.status(404).send();
+        }
+    }, function() {
+        res.status(500).send();
+    })
 });
 
 db.sequelize.sync().then(function () {
